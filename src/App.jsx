08a018ef7942c4e003cee5ad1f2a7251b6ad97e3
Chyisiang <chyisiang8@gmail.com>
2023-09-29 11:46:46 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,59 @@
-import { useState } from 'react'
-import './App.css'
-import MovieList from './components/MovieList'
+import React, { useState, useEffect } from "react";
+import MovieList from "./components/MovieList";
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [movieData, setMovieData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const apiKey = "b096d6cb";
+        const response = await fetch(
+          `https://www.omdbapi.com/?apikey=${apiKey}&s=${query}&r=json`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        if (data.Search) {
+          setMovieData(data.Search.slice(0, 10));
+          setError(null);
+        } else {
+          setMovieData([]);
+          setError("No movies found.");
+        }
+      } catch (error) {
+        console.error("An error occurred while fetching data:", error);
+        setError("An error occurred while fetching data.");
+      }
+    };
+
+    if (query.trim() !== "") {
+      fetchMovies();
+    } else {
+      setMovieData([]);
+      setError(null);
+    }
+  }, [query]);
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
 
   return (
-    <>
-      <MovieList />
-    </>
-  )
-}
+    <div>
+      <input
+        type="text"
+        placeholder="Search for movies..."
+        value={query}
+        onChange={handleInputChange}
+      />
+      {error && <p>{error}</p>}
+      <MovieList movies={movieData} />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,41 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const MovieList = ({ movies }) => {
-    const [movieData, setMovieData] = useState([]);
+  const [movieData, setMovieData] = useState([]);
 
-    useEffect(() => {
-        const fetchMovies = async () => {
-            try {
-                const apiKey = 'b096d6cb';
-                const response = await fetch(
-                    `https://www.omdbapi.com/?apikey=${apiKey}&s=${movies}&r=json`
-                );
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
-                if (data.Search) {
-                    setMovieData(data.Search.slice(0, 10));
-                } else {
-                    setMovieData([]);
-                }
-            } catch (error) {
-                console.error('An error occurred while fetching data:', error);
-            }
-        };
+  useEffect(() => {
+    setMovieData(movies.slice(0, 10));
+  }, [movies]);
 
-        fetchMovies();
-    }, [movies]);
-
-    return (
-        <div>
-            <ul>
-                {movieData.map((movie) => (
-                    <li key={movie.imdbID}>{movie.Title}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul>
+        {movieData.map((movie) => (
+          <li key={movie.imdbID}>{movie.Title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MovieList;
